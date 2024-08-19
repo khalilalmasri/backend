01ecd85e8fb165f379c1cfc8c3b6e14fb0da13e2
khalilalmasri <96783293+khalilalmasri@users.noreply.github.com>
2024-08-19 21:07:19 +0300

    Refactor post controller and routes to include new functionalities
    - Added functionality to get all posts, get a single post by ID, and get the count of posts.
    - Updated post routes to include new endpoints for fetching all posts, a single post, and the count of posts.
    - Implemented pagination for fetching all posts, allowing for better performance and user experience.
    - Included category filtering for fetching posts, enhancing the search capabilities.
    - Ensured proper error handling and validation for post creation, including image and data validation.
    - Optimized the code structure for better readability and maintainability.

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,7 +2,10 @@ const fs = require("fs");
 const path = require("path");
 const asyncHandler = require("express-async-handler");
 const { Post, validateCreatePost } = require("../model/Post");
-const {cloudinaryUploadImg, cloudinaryRemoveImg} = require("../utils/cloudinary");
+const {
+  cloudinaryUploadImg,
+  cloudinaryRemoveImg,
+} = require("../utils/cloudinary");
 
 /**----------------------------------------------
 * @description  Create new Post
@@ -11,45 +14,101 @@ const {cloudinaryUploadImg, cloudinaryRemoveImg} = require("../utils/cloudinary"
 * @access private (only logged in user)
 ------------------------------------------------*/
 
-
 module.exports.createPostCtrl = asyncHandler(async (req, res) => {
-    // 1 . validation for image
-    if (!req.file) {
-        return res.status(400).json({ message: "File not found no image provided" });
-    }
-    // 2 . validation for data
-    const { error } = validateCreatePost(req.body);
-    if (error) {
-        return res.status(400).json({ message: error.details[0].message });
-    }
-    // 3 . upload image
-    const imagePath = path.join(__dirname, `../images/${req.file.filename}`);
-    const result = await cloudinaryUploadImg(imagePath);
-    // 4 . create post and save it in db
-    // ******this way
-    // const newPost = new Post({
-    //     title: req.body.title,
-    // });
-    // await newPost.save();
+  // 1 . validation for image
+  if (!req.file) {
+    return res
+      .status(400)
+      .json({ message: "File not found no image provided" });
+  }
+  // 2 . validation for data
+  const { error } = validateCreatePost(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  // 3 . upload image
+  const imagePath = path.join(__dirname, `../images/${req.file.filename}`);
+  const result = await cloudinaryUploadImg(imagePath);
+  // 4 . create post and save it in db
+  // ******this way
+  // const newPost = new Post({
+  //     title: req.body.title,
+  // });
+  // await newPost.save();
 
-    //*****another way
-    const newPost = await Post.create({
-        title: req.body.title,
-        description: req.body.description,
-        category: req.body.category,
-        image: {
-            url: result.secure_url,
-            publicId: result.public_id,
-        },
-        user: req.user.id,
-    })
+  //*****another way
+  const newPost = await Post.create({
+    title: req.body.title,
+    description: req.body.description,
+    category: req.body.category,
+    image: {
+      url: result.secure_url,
+      publicId: result.public_id,
+    },
+    user: req.user.id,
+  });
 
-    // 5 . send response to the client
-    res.status(200).json({
-        message: "Post created successfully",
-        post: newPost,
-    });
-    // 6 . remove old image from the server
-    fs.unlinkSync(imagePath);
-    
+  // 5 . send response to the client
+  res.status(200).json({
+    message: "Post created successfully",
+    post: newPost,
+  });
+  // 6 . remove old image from the server
+  fs.unlinkSync(imagePath);
 });
+
+/**----------------------------------------------
+* @description  Get all Post
+* @route /api/posts
+* @method Get
+* @access public 
+------------------------------------------------*/
+module.exports.getAllPostsCtrl = asyncHandler(async (req, res) => {
+  const POST_PER_PAGE = 3;
+  const { pageNumper, category } = req.query;
+  let posts;
+  if (pageNumper) {
+    posts = await Post.find()
+      .skip((pageNumper - 1) * POST_PER_PAGE)
+      .limit(POST_PER_PAGE)
+      .sort({ createdAt: -1 })
+      .populate("user", ["-password"]);
+  } else if (category) {
+    posts = await Post.find({ category })
+      .sort({ createdAt: -1 })
+      .populate("user", ["-password"]);
+  } else {
+    posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .populate("user", ["-password"]);
+  }
+  res.status(200).json({ posts });
+});
+
+/**----------------------------------------------
+* @description  Get single Post
+* @route /api/posts/:id
+* @method GET
+* @access public 
+------------------------------------------------*/
+module.exports.getSinglePostsCtrl = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id).populate("user", [
+    "-password",
+  ]);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+
+  res.status(200).json(post);
+});
+
+/**----------------------------------------------
+* @description  Get posts Count
+* @route /api/posts/count
+* @method GET
+* @access public 
+------------------------------------------------*/
+module.exports.getPostsCountCtrl = asyncHandler(async (req, res) => {
+    const count = await Post.countDocuments();
+    res.status(200).json(count);
+  });
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,11 +1,24 @@
 const router = require("express").Router();
-const { createPostCtrl } = require("../controllers/postController");
+const {
+  createPostCtrl,
+  getAllPostsCtrl,
+  getSinglePostsCtrl,
+  getPostsCountCtrl,
+} = require("../controllers/postController");
 const photoupload = require("../middlewares/PhotoUpload");
-const {verifyToken} = require("../middlewares/verifyToken");
-
+const validateObjectId = require("../middlewares/validateObjectId");
+const { verifyToken } = require("../middlewares/verifyToken");
 
 // /api/posts
-router.route("/").post(verifyToken, photoupload.single("image"),createPostCtrl);
+router
+  .route("/")
+  .post(verifyToken, photoupload.single("image"), createPostCtrl)
+  .get(getAllPostsCtrl);
 
+// /api/posts/count
+router.route("/count").get(getPostsCountCtrl);
 
-module.exports = router
+// /api/posts/:id
+router.route("/:id").get(validateObjectId, getSinglePostsCtrl);
+
+module.exports = router;

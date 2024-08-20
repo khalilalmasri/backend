bb6fd3592b7bb15367a3260e678700b2afd7a667
khalilalmasri <96783293+khalilalmasri@users.noreply.github.com>
2024-08-20 14:58:27 +0300

    Add Post Update Functionality and Image Update Endpoint
    This commit introduces the ability to update both the content and the image of a post in the application. It includes the implementation of two new controller functions: `updatePostCtrl` for updating post details and `updatePostImageCtrl` for updating the post image. Additionally, corresponding routes have been added to the `postRoute.js` file to handle PUT requests for updating post content and images.
    
    The `updatePostCtrl` function validates the incoming data, checks if the post exists, verifies if the user is the owner of the post, and then updates the post details accordingly. The `updatePostImageCtrl` function similarly checks for post existence and ownership, deletes the old image from Cloudinary, uploads the new image, updates the post's image field in the database, and finally

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 const asyncHandler = require("express-async-handler");
-const { Post, validateCreatePost } = require("../model/Post");
+const {
+  Post,
+  validateCreatePost,
+  validateUpdatePost,
+} = require("../model/Post");
 const {
   cloudinaryUploadImg,
   cloudinaryRemoveImg,
@@ -134,4 +138,75 @@ module.exports.DeletePostsCtrl = asyncHandler(async (req, res) => {
   } else {
     return res.status(403).json({ message: "Not authorized" });
   }
+});
+
+/**----------------------------------------------
+* @description  Update Post
+* @route /api/posts/:id
+* @method PUT
+* @access private (only owner)
+------------------------------------------------*/
+module.exports.updatePostCtrl = asyncHandler(async (req, res) => {
+  // 1 . validation for data
+  const { error } = validateUpdatePost(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  // 2 . check if post exist
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  // 3 . check if user is owner
+  if (req.user.id !== post.user.toString()) {
+    return res.status(403).json({ message: "Not authorized" });
+  }
+  // 4 . update post
+  const { title, description, category } = req.body;
+  const updatedPost = await Post.findByIdAndUpdate(
+    req.params.id,
+    { $set: { title, description, category } },
+    { new: true }
+  ).populate("user", ["-password"]);
+
+  // 5 . send response to the client
+  res.status(200).json(updatedPost);
+});
+/**----------------------------------------------
+* @description  Update Post Image
+* @route /api/posts/upload-image/:id
+* @method PUT
+* @access private (only owner)
+------------------------------------------------*/
+module.exports.updatePostImageCtrl = asyncHandler(async (req, res) => {
+  // 1 . validation for data
+  if (!req.file) {
+    return res.status(400).json({ message: "no image was uploaded " });
+  }
+  // 2 . check if post exist
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  // 3 . check if user is owner
+  if (req.user.id !== post.user.toString()) {
+    return res.status(403).json({ message: "Not authorized" });
+  }
+  // 4 . delete old image
+  await cloudinaryRemoveImg(post.image.publicId);
+  // 5 . upload new image
+  const imagePath = path.join(__dirname, `../images/${req.file.filename}`);
+  const result = await cloudinaryUploadImg(imagePath);
+  // 6 . update the image field in the DB
+  const updatedPost = await Post.findByIdAndUpdate(
+    req.params.id,
+    { $set: { image: { url: result.secure_url, publicId: result.public_id } } },
+    { new: true }
+  ).populate("user", ["-password"]);
+
+  // 7 . send response to the client
+  res.status(200).json(updatedPost);
+
+  // 8 . remove image from server
+  fs.unlinkSync(imagePath);
 });
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,6 +5,8 @@ const {
   getSinglePostsCtrl,
   getPostsCountCtrl,
   DeletePostsCtrl,
+  updatePostCtrl,
+  updatePostImageCtrl,
 } = require("../controllers/postController");
 const photoupload = require("../middlewares/PhotoUpload");
 const validateObjectId = require("../middlewares/validateObjectId");
@@ -23,6 +25,17 @@ router.route("/count").get(getPostsCountCtrl);
 router
   .route("/:id")
   .get(validateObjectId, getSinglePostsCtrl)
-  .delete(validateObjectId, verifyToken, DeletePostsCtrl);
+  .delete(validateObjectId, verifyToken, DeletePostsCtrl)
+  .put(validateObjectId, verifyToken, updatePostCtrl);
+
+// /api/posts/update-image/:id
+router
+  .route("/update-image/:id")
+  .put(
+    validateObjectId,
+    verifyToken,
+    photoupload.single("image"),
+    updatePostImageCtrl
+  );
 
 module.exports = router;
